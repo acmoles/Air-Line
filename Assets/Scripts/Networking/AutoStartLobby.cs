3dbae0615e84310b4ef7ddf29697357861260218
Anthony Moles <acmoles@gmail.com>
2022-06-03 15:18:11 +0100

    adding missing file

---
+++ Assets/Scripts/Networking/AutoStartLobby.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+ public static class AutoStartLobby
+ {
+     private const string settingMenuPath = "AirLine/Lobby On Start";
+     private const string SettingName = "LobbyOnStart";
+ 
+     public static bool IsEnabled
+     {
+         get { return EditorPrefs.GetBool(SettingName, true); }
+         set { EditorPrefs.SetBool(SettingName, value); }
+     }
+          
+     [MenuItem(settingMenuPath)]
+     private static void ToggleAction()
+     {
+         IsEnabled = !IsEnabled;
+     }
+  
+     [MenuItem(settingMenuPath, true)]
+     private static bool ToggleActionValidate()
+     {
+         Menu.SetChecked(settingMenuPath, IsEnabled);
+         return true;
+     }
+ }
